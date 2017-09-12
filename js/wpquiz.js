d2a1b06d384aee7d18f55b2f324d87bca64e146e
aschweigert <aschweigert@gmail.com>
2017-09-12 11:24:30 -0400

    address issues identified by phpcs vip ruleset

--- js/wpquiz.js
+++ js/wpquiz.js
@@ -1,12 +1,11 @@
-
 /**
- * Checks for divs with a .quizbox tag and 
+ * Checks for divs with a .quizbox tag and
  * creates a new MotherJones quiz in them.
  */
-jQuery(document).ready(function() {
+jQuery( document ).ready(function() {
 
-	jQuery(".news-quiz-quizbox").each( function() {
-		jQuery(this).quiz(jQuery(this).attr('data-key')); 
+	jQuery( ".news-quiz-quizbox" ).each( function() {
+		jQuery( this ).quiz( jQuery( this ).attr( 'data-key' ) );
 	});
 
 });
